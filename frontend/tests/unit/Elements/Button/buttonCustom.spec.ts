5615fc78192f39250f54b32636b23bf29704ce34
wertey <verteyko1990@gmail.com>
2021-10-16 19:12:02 +0300

    unit tests

--- frontend/tests/unit/Elements/Button/buttonCustom.spec.ts
+++ frontend/tests/unit/Elements/Button/buttonCustom.spec.ts
@@ -1,29 +1,39 @@
 import { shallowMount } from '@vue/test-utils';
 import ButtonCustom from '@/components/Elements/Button/ButtonCustom.vue';
 
-/** Определяем входные параметры */
-const slots = { default: 'ButtonText' };
-const defaultBtnType = 'button';
-const propData = { type: 'submit' };
-const listeners = { click: null };
-
-// let wrapper;
-//
-// const createComponent = (options: any) => {
-//   wrapper = shallowMount(ButtonCustom, options);
-// };
-//
-// beforeEach(() => {
-//   listeners.click = jest.fn();
-// });
-//
-// afterEach(() => {
-//   wrapper.destry();
-// });
-
 describe('ButtonCustom.vue', () => {
+  /** Определяем входные параметры */
+  const slots = { default: 'ButtonText' };
+  const defaultBtnType = 'button';
+  const propData = { type: 'submit' };
+  const listeners = { click: null };
+
+  let wrapper: any;
+
+  const createComponent = (options: any) => {
+    wrapper = shallowMount(ButtonCustom, options);
+  };
+
+  beforeEach(() => {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    listeners.click = jest.fn();
+  });
+
+  afterEach(() => {
+    if (wrapper) {
+      wrapper.destry();
+    }
+  });
+
+  test('raise the click event on click', () => {
+    createComponent({ listeners });
+    wrapper.find('.app__button').trigger('click');
+    expect(listeners.click).toHaveBeenCalled();
+  });
+
   test('make ButtonCustom snapshot', () => {
-    const wrapper = shallowMount(ButtonCustom);
+    wrapper = shallowMount(ButtonCustom);
     expect((wrapper.element)).toMatchSnapshot();
   });
 });
